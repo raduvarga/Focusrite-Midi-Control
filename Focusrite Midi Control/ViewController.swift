8c8a01d9c4fbf3a6fcc2bc9d4bbbe5003f8de0bd
Antonio-Radu Varga <antonio-radu@toptal.com>
2018-07-07 15:52:41 +0300

    Initial Commit

---
+++ Focusrite Midi Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Focusrite Midi Control
+//
+//  Created by Antonio-Radu Varga on 07.07.18.
+//  Copyright © 2018 Antonio-Radu Varga. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
