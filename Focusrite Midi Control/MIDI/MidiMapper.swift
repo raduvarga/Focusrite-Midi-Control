21fe74b116dbcb31289cea8477fbc2f76ee17809
Antonio-Radu Varga <antonio-radu@toptal.com>
2018-07-14 23:56:24 +0300

    reorganized folders

---
+++ Focusrite Midi Control/MIDI/MidiMapper.swift
@@ -0,0 +1,13 @@
+//
+//  MidiMapper.swift
+//  Focusrite Midi Control
+//
+//  Created by Antonio-Radu Varga on 08.07.18.
+//  Copyright © 2018 Antonio-Radu Varga. All rights reserved.
+//
+
+import Cocoa
+
+class MidiMapper: NSObject {
+
+}
